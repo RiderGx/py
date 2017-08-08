2378ec3633e219ab971c4ffe2715395b6dd337f6
RiderGx <gh605149773@gmail.com>
2017-08-08 18:32:43 +0800

    新建班级  以及班级计数器

--- school/school.py
+++ school/school.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
-class SchoolMember(object): #主校北京
-    def __init__(self,name,age,course):
+import json
+class SchoolMember(object): #学校模板
+    def __init__(self,name,course):
         self.name = name
-        self.age = age
         self.course = course
+
     def teaching(self):
-        if self.course == 'c++':
+        if self.course == 'linux' or self.course == 'python':
             School_bj.tell(self)
+        if self.course == 'go':
+            School_sh.School_list(self)
+        else:
+            print('输入错误')
     # def enroll(self):
     #     '''注册'''
     #     print('new school number [%s]'% self.name)
@@ -18,53 +23,56 @@ class SchoolMember(object): #主校北京
     #         print('\t',k,v)
 
 class School_bj(SchoolMember):
-
+    '''定义了北京的分校'''
     def __init__(self,add):
-        super(School_bj,self).__init__(name,age,course)
+        super(School_bj,self).__init__(name,course)
         self.add = add
 
     def tell(self):
         print('%s 老师在北京任课，教%s'%(self.name,self.course))
 
-    #
+
     # def open_branch(self,addr):
     #     print('开分校在[%s]'%addr)
+class School_sh(SchoolMember):
+    '''定义了上海的分校'''
 
-# class Teacher(SchoolMember):
-#     def __init__(self,name,age,salary,course,school_obj):
-#         # SchoolMember.__init__(self,name,age,sex)
-#         super(Teacher,self).__init__(name,age,sex)
-#         self.school = school_obj
-#         self.salary = salary
-#         self.course = course
-#         self.enroll()#直接调用
-#
-#     def teaching(self):
-#         print('Teacher [%s] is teaching [%s]'%(self.name,self.course))
+    def __init__(self):
+        super(School_bj,self).__init__(name,course)
+        self.class_list = []
 
-# class Studenet(SchoolMember):
-#     def __init__(self,name,age,course,tuition):
-#         SchoolMember.__init__(self,name,age,sex)
-#         self.course = course
-#         self.tuition = tuition
-#         self.enroll()
-#         self.amount = 0
-#
-#     def pay_tuiton(self,amount):
-#         print('student [%s] has just paied [%s]'%(self.name,amount))
-#         self.amount += amount'c++'
+    def School_list(self):
+        School_sh_list = input('选择查看')
+        if School_sh_list == 'a':
+            return School_sh.creat_class(self)
+
+    # def tell(self):
+    #     print('%s 老师在上海任课，教%s'%(self.name,self.course))
+
+    def class_list_add(self):
+        self.class_list.append(self.name)
+
+    def creat_class(self):
+        with open('class_number','r',encoding='utf-8') as class_list_number:
+            class_list_number = class_list_number.read()
+            class_name =  self.name + '\t'+'go' + '\t'+class_list_number
+            class_personnel = {'teacher':self.name,'student':[]}
+            with open('%s.json'%(class_name) , 'w', encoding='utf-8') as class_name_file:
+                json.dump(class_personnel,class_name_file)
+            class_list_number = int(class_list_number)
+            class_list_number += 1
+            class_list_number = str(class_list_number)
+            with open('class_number', 'w', encoding='utf-8') as new_class_list_number:
+                new_class_list_number.write(class_list_number)
+
+
+
+
 
 name = 'a'
-age = '18'
-course =
+course = input('输入教学学科').strip()
 
-t1 = SchoolMember(name,age,course)
+
+
+t1 = SchoolMember(name,course)
 t1.teaching()
-
-#
-# t1 = Teacher('a',18,'F**M',3000,'python','SH')
-# s1 = Studenet('b',19,'N/A','PYS16',30000)
-# s2 = Studenet('c',22,'M','PYS15',11000)
-#
-#
-# s1.pay_tuiton(1)
