0f1aabeeffb90f82e3a9d86677b1bbf31d0dfa16
RiderGx <gh605149773@gmail.com>
2017-08-22 18:35:21 +0800

    路径

--- Ftp/server.py
+++ Ftp/server.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
+import json
+
+
+
+
 class Ftp_server(object):
     def __init__(self,name,passwd):
         self.name = name
@@ -6,20 +12,24 @@ class Ftp_server(object):
 
     def User_Authentication(self):
         if self.name == 'root' and self.passwd == 'root':
-            Ftp_root.print_self(self,a)
+            Ftp_root()
         else:
             pass #跳到用户模式
 
 class Ftp_root(Ftp_server):
     def __init__(self):
-        super(Ftp_root,self).__init__(name,passwd,locasf)
+        super(Ftp_root,self).__init__(name,passwd)
+        locasf = r"\%s\%s"%(os.getcwd(),self.name)
         self.locasf = locasf
+        return Ftp_root.print_self(self)
 
     def print_self(self):
-        print(self.name)
-        username = input('新的姓名')
-        self.name = username
-        print(self.name,self.passwd)
+        print(self.locasf)
+        # username = input('新的姓名')
+        # self.name = username
+        # os.mkdir(self.name)
+
+
 
 name = input('姓名:')
 passwd = input('密码:')
