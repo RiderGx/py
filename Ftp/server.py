e1ab41918589beeaa87be8f87dbc3e57807ce73f
RiderGx <605149773@qq.com>
2017-08-22 22:58:57 +0800

    用户注册完结

--- Ftp/server.py
+++ Ftp/server.py
@@ -3,8 +3,6 @@ import os
 import json
 
 
-
-
 class Ftp_server(object):
     def __init__(self,name,passwd):
         self.name = name
@@ -14,21 +12,45 @@ class Ftp_server(object):
         if self.name == 'root' and self.passwd == 'root':
             Ftp_root()
         else:
-            pass #跳到用户模式
+            try:
+                user_ =  open("%s\\user\%s.json"%(os.getcwd(),self.name), 'r')
+            except FileNotFoundError:
+                print('用户名不存在')
+            else:
+                user_data = json.load(user_)
+                if self.name == user_data['username'] and self.passwd == user_data['password']:
+                    return Ftp_user()
+                    return user_data
+
+                else:
+                    print('账号密码错误')
+                    exit()
 
 class Ftp_root(Ftp_server):
     def __init__(self):
         super(Ftp_root,self).__init__(name,passwd)
-        locasf = r"\%s\%s"%(os.getcwd(),self.name)
-        self.locasf = locasf
         return Ftp_root.print_self(self)
 
     def print_self(self):
-        print(self.locasf)
-        # username = input('新的姓名')
-        # self.name = username
-        # os.mkdir(self.name)
+        username = input('输入用户>>>:').strip()
+        path = "%s\\user\%s.json" % (os.getcwd(), username)
+        locasf = r"\Ftp\%s" % (username)
+        self.locasf = locasf
+        if os.path.isfile(path) == 'True':
+            print('用户存在')
+        else:
+            passwd = input('输入用户密码>>>：').strip()
+            self.name = username
+            user_dict = {"username":self.name,"password":passwd,"locasf":[locasf]}
+            os.mkdir(self.name)
+            with open(path,'w',encoding='utf-8') as user_w:
+                json.dump(user_dict,user_w)
+                exit()
 
+class Ftp_user(Ftp_server):
+    def __init__(self):
+        super(Ftp_user,self).__init__(name,passwd)
+        self.user_data = user_data
 
 
 name = input('姓名:')
